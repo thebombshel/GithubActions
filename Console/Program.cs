1334f4616483fbf5e32578944537e171e4bcc56b
thebombshel <89432673+thebombshel@users.noreply.github.com>
2023-04-12 17:58:36 -0500

    Update Program.cs

--- Console/Program.cs
+++ Console/Program.cs
@@ -84,7 +84,11 @@ namespace GithubActionsLab
         // Implement this method following a similar pattern as above
         public static double Power(string x, string y)
         {
-            throw new NotImplementedException();
+            double j = double.Parse(x);
+            for (int i = 1; i <= int.parse(y); i++) {
+                j*= double.Parse(x);
+            }
+            return j;
         }
     }
 
